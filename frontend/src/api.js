a78709db98b6ada4be15cf7dddc58db40a2ae7b1
nori-1234 <nurzanturehan66@gmail.com>
2026-04-07 12:40:53 +0600

    Fix API URL to deployed backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const API_URL = 'https://forum-project-4jzk.onrender.com/api';
+
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: API_URL,
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json'
